92914cfbf19c47c93bda52938574c6f145145ddc
ispipa <ispipa123@gmail.com>
2022-10-11 17:34:32 +0200

    Updates

---
+++ nav.js
@@ -0,0 +1,45 @@
+/*$(".nav-item").on("click", function(){
+	$(".nav-item.active").removeClass("active");
+	$(this).addClass("active");
+});
+
+$(".nav-item").on("click", function(){
+    if(window.innerWidth <= 991)
+    {
+        $(".navbar-collapse.collapse.show").removeClass("show");
+        $(".nav-header").removeClass("navStrict");
+        $(".navbar-toggler").addClass("collapsed");
+    }
+});
+$(".navbar-toggler").on("click", function()
+{
+    if(window.innerWidth <= 991)
+    {
+        $(".nav-header").toggleClass("navStrict");
+    }
+});
+$(window).resize(function() 
+{
+    if(window.innerWidth  <= 991)
+    {
+            $(".nav-header").removeClass("navStrict");
+    }
+    else{
+        $(".nav-header").addClass("navStrict");
+    }
+});
+
+
+$(window).on('scroll', function() 
+{
+    if(window.innerWidth >= 992)
+    {
+        console.log("985");
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            $(".nav-header").addClass("navStrict");
+        } else {
+            $(".nav-header").removeClass("navStrict");
+        }
+    }
+});*/
